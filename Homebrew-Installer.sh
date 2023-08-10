4f7029baecdbf0af2d7b1f331814295382a4c5ce
jrjflint <james.follent@gmail.com>
2023-08-10 15:17:26 +1000

    Adjusted syntax and changed log file location

--- Homebrew-Installer.sh
+++ Homebrew-Installer.sh
@@ -1,19 +1,26 @@
 #!/bin/bash
 
 # Define the log file
-LOG_FILE="/var/log/homebrew_install.log"
+LOG_FILE="./log/homebrew_install.log"
 
 # Function to log messages
 log_message() {
     echo "$1" | tee -a "$LOG_FILE"
 }
 
+if [ -d "./log" ]; then
+    log_message "log folder already exists"
+else
+    mkdir ./log
+    log_message "log folder created"
+fi
+
 # Check if Homebrew is installed
 if ! command -v brew &> /dev/null; then
     log_message "Installing Homebrew..."
 
     # Install Homebrew
-    if /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &>> "$LOG_FILE"; then
+    if /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)" &> "$LOG_FILE"; then
         log_message "Homebrew successfully installed!"
     else
         log_message "Failed to install Homebrew. Check $LOG_FILE for more details."
@@ -39,8 +46,8 @@ packages=(
 # Install the packages
 for package in "${packages[@]}"; do
     log_message "Installing $package..."
-    
-    if brew install --cask "$package" &>> "$LOG_FILE"; then
+    brew list "$package" || brew install cask "$package"
+    if brew install --cask "$package" &> "$LOG_FILE"; then
         log_message "$package successfully installed!"
     else
         log_message "Failed to install $package. Check $LOG_FILE for more details."
